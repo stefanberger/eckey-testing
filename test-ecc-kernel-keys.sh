7519722fbad10c56d7e0043ca014595c6d8d373b
Stefan Berger <stefanb@linux.ibm.com>
2021-04-30 12:45:23 -0400

    Reformat test-ecc-kernel-keys.sh
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- test-ecc-kernel-keys.sh
+++ test-ecc-kernel-keys.sh
@@ -89,50 +89,50 @@ main() {
   while :; do
     for curve in $(echo ${curves}); do
       for hash in sha1 sha224 sha256 sha384 sha512; do
-         certfile="cert.der"
-         openssl req \
-                -x509 \
-		-${hash} \
-		-newkey ec \
-		-pkeyopt ec_paramgen_curve:${curve} \
-		-keyout key.pem \
-		-days 365 \
-		-subj '/CN=test' \
-		-nodes \
-		-outform der \
-		-out ${certfile} 2>/dev/null
+        certfile="cert.der"
+        openssl req \
+        	-x509 \
+        	-${hash} \
+        	-newkey ec \
+        	-pkeyopt ec_paramgen_curve:${curve} \
+        	-keyout key.pem \
+        	-days 365 \
+        	-subj '/CN=test' \
+        	-nodes \
+        	-outform der \
+        	-out ${certfile} 2>/dev/null
 
-		exp=0
-		# Every once in a while we inject a fault into the
-		# certificate's key or signature
-		case $((RANDOM & 255)) in
-		255)
-			inject_fault_cert_key "${certfile}" "${certfile}.bad"
-			certfile="${certfile}.bad"
-			exp=1
-			;;
-		254)
-			inject_fault_cert_signature "${certfile}" "${certfile}.bad"
-			certfile="${certfile}.bad"
-			exp=1
-			;;
-		esac
+        exp=0
+        # Every once in a while we inject a fault into the
+        # certificate's key or signature
+        case $((RANDOM & 255)) in
+        255)
+          inject_fault_cert_key "${certfile}" "${certfile}.bad"
+          certfile="${certfile}.bad"
+          exp=1
+        ;;
+        254)
+          inject_fault_cert_signature "${certfile}" "${certfile}.bad"
+          certfile="${certfile}.bad"
+          exp=1
+        ;;
+        esac
 
-		id=$(keyctl padd asymmetric testkey %keyring:test < "${certfile}")
-		rc=$?
-		if [ $rc -ne $exp ]; then
-			case "$exp" in
-			0) echo "Error: Could not load valid certificate!";;
-			1) echo "Error: Succeeded to load invalid certificate!";;
-			esac
-			echo "curve: $curve hash: $hash"
-			exit 1
-		else
-			case "$rc" in
-			0) printf "Good: curve: %10s hash: %-7s keyid: %-10s\n" $curve $hash $id;;
-			*) printf "Good: curve: %10s hash: %-7s keyid: %-10s -- bad certificate was rejected\n" $curve $hash $id;;
-			esac
-		fi
+        id=$(keyctl padd asymmetric testkey %keyring:test < "${certfile}")
+        rc=$?
+        if [ $rc -ne $exp ]; then
+          case "$exp" in
+          0) echo "Error: Could not load valid certificate!";;
+          1) echo "Error: Succeeded to load invalid certificate!";;
+          esac
+          echo "curve: $curve hash: $hash"
+          exit 1
+        else
+          case "$rc" in
+          0) printf "Good: curve: %10s hash: %-7s keyid: %-10s\n" $curve $hash $id;;
+          *) printf "Good: curve: %10s hash: %-7s keyid: %-10s -- bad certificate was rejected\n" $curve $hash $id;;
+          esac
+        fi
       done
     done
   done
